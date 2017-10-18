45660d1f4c6931f918ed9665c4ad607bd18f0bd9
Kent Watsen <kent@watsen.net>
2017-10-18 00:45:28 -0400

    this is -03

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -1,10 +1,24 @@
 
-echo "Testing YANG syntax..."
+echo "Testing ietf-keystore.yang..."
 pyang --ietf --max-line-length=70 -p ../ ../ietf-keystore\@*.yang
 pyang --canonical -p ../ ../ietf-keystore\@*.yang
+yanglint ../ietf-keystore\@*.yang
 
-echo "Testing keystore module..."
-./validate.sh ../ietf-keystore\@*.yang ex-keystore.xml
-#./validate.sh ../ietf-keystore\@*.yang ex-keystore-rpc-gpk-restconf-json.xml
-#./validate.sh ../ietf-keystore\@*.yang ex-keystore-rpc-gcsr-netconf.xml
+echo "Testing ex-keystore.xml..."
+yanglint -s ../ietf-keystore\@*.yang ex-keystore.xml
+
+echo "Testing ex-keystore-gpk-rpc.xml..."
+yanglint -t auto -s ../ietf-keystore\@*.yang ex-keystore-gpk-rpc.xml
+
+echo "Testing ex-keystore-gpk-rpc-reply.xml..."
+yanglint -t auto -s ../ietf-keystore\@*.yang ex-keystore-gpk-rpc-reply.xml ex-keystore-gpk-rpc.xml
+
+echo "Testing ex-keystore-gcsr-rpc.xml..."
+yanglint -t auto -s ../ietf-keystore\@*.yang ex-keystore-gcsr-rpc.xml
+
+echo "Testing ex-keystore-gcsr-rpc-reply.xml..."
+yanglint -t auto -s ../ietf-keystore\@*.yang ex-keystore-gcsr-rpc-reply.xml ex-keystore-gcsr-rpc.xml
+
+echo "Testing ex-keystore-ce-notification.xml..."
+yanglint -r ex-keystore.xml -t auto -s ../ietf-keystore\@*.yang ex-keystore-ce-notification.xml
 
