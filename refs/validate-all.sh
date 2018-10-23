2424f051e2fcf9d4236cd4991f1baaa5d9823187
Kent Watsen <kent@watsen.net>
2018-10-22 20:12:50 -0400

    this is -07

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -10,10 +10,10 @@ yanglint ../ietf-keystore\@*.yang
 echo "Testing ex-keystore.xml..."
 yanglint -s ../ietf-*\@*.yang ./ietf-origin.yang ex-keystore.xml
 
-echo "Testing ex-keystore-ce-notification.xml..."
-echo -e 'setns a=urn:ietf:params:xml:ns:neteonf:notification:1.0\nsetns b=urn:ietf:params:xml:ns:yang:ietf-keystore\ncat //a:notification/b:keystore' | xmllint --shell ex-keystore-ce-notification.xml | sed -e '/^\/.*/d' -e '/^ *$/d' > yanglint-notification.xml
-yanglint -s -t notif -r ex-keystore.xml ../ietf-*\@*.yang yanglint-notification.xml
-rm yanglint-notification.xml
+#echo "Testing ex-keystore-ce-notification.xml..."
+#echo -e 'setns a=urn:ietf:params:xml:ns:neteonf:notification:1.0\nsetns b=urn:ietf:params:xml:ns:yang:ietf-keystore\ncat //a:notification/b:keystore' | xmllint --shell ex-keystore-ce-notification.xml | sed -e '/^\/.*/d' -e '/^ *$/d' > yanglint-notification.xml
+#yanglint -s -t notif -r ex-keystore.xml ../ietf-*\@*.yang yanglint-notification.xml
+#rm yanglint-notification.xml
 
 echo "Testing ex-keystore-usage.yang (pyang)..."
 pyang --lint --max-line-length=69 -p ../ ../ex-keystore-usage\@*.yang
@@ -24,21 +24,21 @@ yanglint ../ex-keystore-usage\@*.yang
 echo "Testing ex-keystore-usage.xml..."
 yanglint -m -s ../ex-keystore-usage\@*.yang ../ietf-*\@*.yang ./ietf-origin.yang  ex-keystore-usage.xml ex-keystore.xml
 
-echo "Testing ex-keystore-ghk-rpc.xml..."
-yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ghk-rpc.xml
+#echo "Testing ex-keystore-ghk-rpc.xml..."
+#yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ghk-rpc.xml
 
-echo "Testing ex-keystore-ghk-rpc-reply.xml..."
-yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ghk-rpc-reply.xml ex-keystore-ghk-rpc.xml
+#echo "Testing ex-keystore-ghk-rpc-reply.xml..."
+#yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ghk-rpc-reply.xml ex-keystore-ghk-rpc.xml
 
-echo "Testing ex-keystore-ihk-rpc.xml..."
-yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ihk-rpc.xml
+#echo "Testing ex-keystore-ihk-rpc.xml..."
+#yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ihk-rpc.xml
 
-echo "Testing ex-keystore-ihk-rpc-reply.xml..."
-yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ihk-rpc-reply.xml ex-keystore-ihk-rpc.xml
+#echo "Testing ex-keystore-ihk-rpc-reply.xml..."
+#yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-ihk-rpc-reply.xml ex-keystore-ihk-rpc.xml
 
-echo "Testing ex-keystore-gcsr-rpc.xml..."
-yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-gcsr-rpc.xml
+#echo "Testing ex-keystore-gcsr-rpc.xml..."
+#yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-gcsr-rpc.xml
 
-echo "Testing ex-keystore-gcsr-rpc-reply.xml..."
-yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-gcsr-rpc-reply.xml ex-keystore-gcsr-rpc.xml
+#echo "Testing ex-keystore-gcsr-rpc-reply.xml..."
+#yanglint -s -t auto ../ietf-*\@*.yang ex-keystore-gcsr-rpc-reply.xml ex-keystore-gcsr-rpc.xml
 
