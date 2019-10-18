1b2da035aa09feb6d2c477204cd7a002348f62ab
Kent Watsen <kent@watsen.net>
2019-10-18 18:32:38 -0400

    this is -13

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -8,7 +8,7 @@ echo "Testing ietf-keystore.yang (yanglint)..."
 yanglint ../ietf-keystore\@*.yang
 
 echo "Testing ex-keystore.xml..."
-yanglint -s ../ietf-keystore\@*.yang ./ietf-origin.yang ex-keystore.xml
+yanglint -s ../ietf-keystore\@*.yang ../ietf-crypto-types\@*.yang ./ietf-origin.yang ex-keystore.xml
 
 #echo "Testing ex-keystore-ce-notification.xml..."
 #echo -e 'setns a=urn:ietf:params:xml:ns:neteonf:notification:1.0\nsetns b=urn:ietf:params:xml:ns:yang:ietf-keystore\ncat //a:notification/b:keystore' | xmllint --shell ex-keystore-ce-notification.xml | sed -e '/^\/.*/d' -e '/^ *$/d' > yanglint-notification.xml
@@ -22,7 +22,7 @@ echo "Testing ex-keystore-usage.yang (yanglint)..."
 yanglint ../ex-keystore-usage\@*.yang
 
 echo "Testing ex-keystore-usage.xml..."
-yanglint -s -m ../ex-keystore-usage\@*.yang ../ietf-keystore\@*.yang ./ietf-origin.yang  ex-keystore-usage.xml ex-keystore.xml
+yanglint -s -m ../ex-keystore-usage\@*.yang ../ietf-keystore\@*.yang ../ietf-crypto-types\@*.yang ./ietf-origin.yang  ex-keystore-usage.xml ex-keystore.xml
 
 echo "Testing ex-generate-symmetric-key-rpc.xml..."
 yanglint -s -t auto -O ex-keystore.xml ../ietf-*\@*.yang ex-generate-symmetric-key-rpc.xml
